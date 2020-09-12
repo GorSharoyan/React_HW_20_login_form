28e67cb77434a5fdc8574f90465fa4505ae94adf
Gor <gor.sharoyan95@gmail.com>
2020-09-12 21:09:06 +0400

    created basic UI /started working on handlers

--- src/App.js
+++ src/App.js
@@ -12,7 +12,18 @@ import TextField from '@material-ui/core/TextField';
             password:'',
         }
     }
-  
+
+  // handleUserame(event)=>{
+  //   if(typeof(event.target.value)!=='string'){
+
+  //   }
+  // }
+
+  // handlePassowrd(event)=>{
+  //   if(event.target.value.spilt('').some){
+
+  //   }
+  // }
     render(){
         return(
      <div>
@@ -20,17 +31,20 @@ import TextField from '@material-ui/core/TextField';
              <label>
                  <h3>Username</h3>
                  <TextField id="outlined-basic" label="Username" variant="outlined" type='text'
-                    value={this.state.step} onChange={this.handleStep} />
+                    value={this.state.username} 
+                    onChange={this.handleUserame} />
              </label>
              <label>
                  <h3>Password</h3>
                  <TextField id="outlined-basic" label="Password" variant="outlined" type='password'
-                    value={this.state.step} onChange={this.handleStep} />
+                   value={this.state.password}
+                    onChange={this.handlePassowrd} />
              </label>
              <hr></hr>
              <label>
-                <Button variant="contained" color="primary" href="#contained-buttons" onClick={this.handleSubmit} >
-                 <h3>Submit</h3>
+                <Button variant="contained" color="primary" href="#contained-buttons" className='btn'
+                onClick={this.handleSubmit} >
+                 <h5>Log In</h5>
                </Button>
                   
              </label>
