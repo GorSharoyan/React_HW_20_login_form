3e6f88c931a963bfbc12b21acebc45220c20acea
Gor <gor.sharoyan95@gmail.com>
2020-09-13 04:02:05 +0400

    worked on event listeners/moved soe data to local storage

--- src/App.js
+++ src/App.js
@@ -13,36 +13,53 @@ import TextField from '@material-ui/core/TextField';
         }
     }
 
-  // handleUserame(event)=>{
-  //   if(typeof(event.target.value)!=='string'){
-
-  //   }
+  handleUserame=(event)=>{
+    // if(typeof(event.target.value)!=='string'){
+    //    throw new Error('invalid username')
+    // }else{
+      this.setState({
+        username:event.target.value
+      })
+    }
   // }
 
-  // handlePassowrd(event)=>{
-  //   if(event.target.value.spilt('').some){
-
-  //   }
+  handlePassowrd=(event)=>{
+    // if(typeof(event.target.value)!=='number'){
+    //     throw new Error('invalid password')
+    // }else{
+      this.setState({
+        password:event.target.value
+      })
+    }
   // }
+
+  handleSubmit=()=>{
+    localStorage.setItem('state',JSON.stringify(this.state))
+  }
+
     render(){
         return(
      <div>
-         <form className='login-form'>
+         <form className='login-form' onSubmit={this.handleSubmit}>
              <label>
                  <h3>Username</h3>
-                 <TextField id="outlined-basic" label="Username" variant="outlined" type='text'
+                 <TextField id="outlined-basic" label="Username"
+                    variant="outlined" type='text'
                     value={this.state.username} 
                     onChange={this.handleUserame} />
              </label>
              <label>
                  <h3>Password</h3>
-                 <TextField id="outlined-basic" label="Password" variant="outlined" type='password'
+                 <TextField id="outlined-basic" label="Password"
+                   variant="outlined" type='password'
                    value={this.state.password}
                     onChange={this.handlePassowrd} />
              </label>
              <hr></hr>
              <label>
-                <Button variant="contained" color="primary" href="#contained-buttons" className='btn'
+                <Button variant="contained" color="primary" 
+                href="#contained-buttons" 
+                className='btn'
                 onClick={this.handleSubmit} >
                  <h5>Log In</h5>
                </Button>
